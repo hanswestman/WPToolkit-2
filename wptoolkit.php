2cf3110ed9312786aa6e6102c4d1df0d583d8f27
Hans Westman <hans@thefarm.se>
2013-10-10 16:30:37 +0200

    Initial plugin file

---
+++ wptoolkit.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: WP Toolkit 2
+ * Plugin URI: https://github.com/hanswestman/WPToolkit-2
+ * Description: A collection of tools to ease the development of plugins and themes.
+ * Author: Hans Westman
+ * Author URI: http://hanswestman.se
+ * Version: 0.1
+ * License: MIT
+ */
+
+define('WPT_VERSION', '0.1');
+define('WPT_PATH_ROOT', dirname(__FILE__));
+
+//load_plugin_textdomain('wpt2', false, WPT_PATH_ROOT . '/languages/');
+
+?>
