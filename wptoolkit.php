bb04fd111ba309ecf76df1268c50203b056f8ae3
Hans Westman <hans@thefarm.se>
2013-10-10 16:38:02 +0200

    Adding the autoloader

--- wptoolkit.php
+++ wptoolkit.php
@@ -10,8 +10,20 @@
  */
 
 define('WPT_VERSION', '0.1');
+define('WPT_TEXTDOMAIN', 'wpt2');
 define('WPT_PATH_ROOT', dirname(__FILE__));
+define('WPT_PATH_LANGUAGES', WPT_PATH_ROOT . '/languages/');
+define('WPT_PATH_MODULES', WPT_PATH_ROOT . '/modules/');
 
-//load_plugin_textdomain('wpt2', false, WPT_PATH_ROOT . '/languages/');
+load_plugin_textdomain(WPT_TEXTDOMAIN, false, WPT_PATH_LANGUAGES);
+
+function wpt2_autoload($class){
+	$file = WPT_PATH_MODULES . $class . '/' . $class . '.class.php';
+	if(file_exists($file)){
+		include_once($file);
+	}
+}
+
+spl_autoload_register('wpt2_autoload');
 
 ?>
