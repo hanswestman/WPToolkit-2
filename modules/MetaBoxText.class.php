f1ef1b7e8df7f5414e2347b48932f46e9b56b704
Hans Westman <hans@thefarm.se>
2013-10-14 10:27:45 +0200

    Added text input class to metabox

---
+++ modules/MetaBoxText.class.php
@@ -0,0 +1,51 @@
+<?php
+
+class MetaBoxText extends MetaBoxBase {
+	
+	var $defaults = array(
+		'type' => 'text',
+		'name' => '',
+		'label' => '',
+		'description' => '',
+		'placeholder' => '',
+		'classes' => array('wpt-input-text'),
+		'style' => '',
+		'default' => '',
+		'multiple' => 'false',
+		'multiple_min' => 1,
+		'multiple_max' => -1,
+	); 
+	
+	var $settings;
+	var $attributes;
+	
+	public function __construct(&$post_id, $inputName, $metaName, $fieldSettings, $inputValue){
+		$this->defaults['name'] = $inputName;
+		$this->settings = $this->SetDefaults($this->defaults, $fieldSettings);
+
+		parent::__construct($post_id, $inputName, $metaName, $fieldSettings, $inputValue);
+		
+		$this->attributes = array(
+			'class' => implode(' ', $this->settings['classes']),
+			'style' => $this->settings['style'],
+			'value' => empty($inputValue) ? $this->settings['default'] : $inputValue,
+			'placeholder' => $this->settings['placeholder'],
+			'type' => 'text',
+			'name' => $inputName,
+			'id' => $inputName,
+		);
+	}
+	
+	public function Render(){
+		$this->RenderWrapperStart();
+		$this->RenderLabel($this->settings['label']);
+		
+		echo('<input' . $this->BuildAttributes($this->attributes) . '>');
+		
+		$this->RenderDescription($this->settings['description']);
+		$this->RenderWrapperEnd();
+	}
+	
+}
+
+?>
